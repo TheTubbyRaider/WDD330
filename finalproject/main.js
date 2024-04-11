c1b12de464a73d862d685f0dd9cd37ada15a547d
TheTubbyRaider <grsraider66@gmail.com>
2024-04-10 21:55:22 -0600

    fixes

---
+++ finalproject/main.js
@@ -0,0 +1,19 @@
+// Import styles.css so it is bundled
+import './styles.css';
+
+// Rest of your main.js code would go here
+
+// For example:
+
+// Select DOM elements
+const btn = document.querySelector('#btn');
+
+// Add click event listener
+btn.addEventListener('click', () => {
+  // Handle click
+});
+
+// Export functions to be used elsewhere
+export function myFunction() {
+  // ...
+}
